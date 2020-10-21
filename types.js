0b7925c07fe047380008849c25acfba261d65ee9
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-10-21 19:45:25 +0200

    Updates

--- types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// TODO: add globally available interfaces for your elements
-// They should probably match the types received from your graphql middleware
-//# sourceMappingURL=types.js.map
