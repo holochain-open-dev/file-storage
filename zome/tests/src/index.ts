529646c2aa40bc12eb3bcbc7353260eaf9ff7f99
hedayat <h.abedijoo@gmail.com>
2020-12-02 16:40:16 +0100

    refactoring test to the latest version of tryorama

--- zome/tests/src/index.ts
+++ zome/tests/src/index.ts
@@ -1,82 +1,107 @@
-import { Orchestrator, Config } from "@holochain/tryorama";
+import {
+  Orchestrator,
+  Config,
+  InstallAgentsHapps,
+  InstalledAgentHapps,
+} from "@holochain/tryorama";
+import { ScenarioApi } from "@holochain/tryorama/lib/api";
+import path from "path";
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
+const conductorConfig = Config.gen();
+
+const conductorHapps: InstallAgentsHapps = [
+  // agent 0 ...
+  [
+    // happ 0
+    [
+      // dna 0
+      path.join("../file_storage.dna.gz"),
+    ],
+  ],
+];
 
 const orchestrator = new Orchestrator();
 
-export const simpleConfig = {
-  alice: Config.dna("../file_storage.dna.gz", null),
-  bobbo: Config.dna("../file_storage.dna.gz", null),
-};
+orchestrator.registerScenario(
+  "get_agent_pubkey test scenario",
+  async (s: ScenarioApi, t) => {
+    const [alice, bob] = await s.players([conductorConfig, conductorConfig]);
+    const [alice_test_happ] = await alice.installAgentsHapps(conductorHapps);
+    const ZOME_NAME = "file_storage";
 
-orchestrator.registerScenario("create a file", async (s, t) => {
-  const { conductor } = await s.players({
-    conductor: Config.gen(simpleConfig),
-  });
-  await conductor.spawn();
-
-  const chunkSize = 8 * 1024;
-  const chunkNumer = 6;
-
-  const bufStr = Array(chunkSize).fill("h").join("");
-  let chunkBytes = Buffer.from(bufStr, "utf8");
-
-  const chunksHashes: any[] = [];
-
-  for (let i = 0; i < chunkNumer; i++) {
-    const start = Date.now();
-
-    const hash = await conductor.call(
-      "alice",
-      "file_storage",
-      "create_file_chunk",
-      Buffer.from(Array(chunkSize).fill(i).join(""))
+    const res = await alice_test_happ[0].cells[0].call(
+      ZOME_NAME,
+      "get_agent_pubkey",
+      null
     );
-    const end = Date.now();
-    console.log(chunkBytes.length);
-    console.log((end - start) / 1000);
-
-    chunksHashes.push(hash);
+    t.ok(res);
   }
+);
 
-  await sleep(3000);
+orchestrator.registerScenario(
+  "create file test scenario",
+  async (s: ScenarioApi, t) => {
+    const [alice, bob] = await s.players([conductorConfig, conductorConfig]);
+    const [alice_test_happ] = await alice.installAgentsHapps(conductorHapps);
+    const ZOME_NAME = "file_storage";
+    const conductor = alice_test_happ[0].cells[0];
 
-  let fileMetadata = {
-    name: "example.txt",
-    fileType: "text/plain",
-    chunksHashes,
-    size: chunkSize * chunkNumer,
-    lastModified: [Math.floor(Date.now() / 1000), 0],
-  };
-  let fileHash = await conductor.call(
-    "alice",
-    "file_storage",
-    "create_file_metadata",
-    fileMetadata
-  );
-  t.ok(fileHash);
+    // In memory dummy file to upload to DNA
+    const chunkSize = 8 * 1024;
+    const chunkNumer = 6;
+    const bufStr = Array(chunkSize).fill("h").join("");
+    let chunkBytes = Buffer.from(bufStr, "utf8");
+    const chunksHashes: any[] = [];
 
-  await sleep(100);
+    ////////Upload each chunk as a file_chunk
+    for (let i = 0; i < chunkNumer; i++) {
+      const start = Date.now();
+      const hash = await conductor.call(
+        ZOME_NAME,
+        "create_file_chunk",
+        Buffer.from(Array(chunkSize).fill(i).join(""))
+      );
+      const end = Date.now();
+      console.log(chunkBytes.length);
+      console.log((end - start) / 1000);
+      chunksHashes.push(hash);
+    }
+    console.log("File Hashes:**********");
+    console.log(chunksHashes);
+    console.log("*********************");
 
-  let fileResult = await conductor.call(
-    "bobbo",
-    "file_storage",
-    "get_file_metadata",
-    fileHash
-  );
-
-  t.ok(fileResult);
-
-  for (const chunkHash of fileResult.chunksHashes) {
-    let chunk = await conductor.call(
-      "alice",
-      "file_storage",
-      "get_file_chunk",
-      chunkHash
+    /////// Upload file Metadata to DNA
+    let fileMetadata = {
+      name: "example.txt",
+      fileType: "text/plain",
+      chunksHashes,
+      size: chunkSize * chunkNumer,
+      lastModified: [Math.floor(Date.now() / 1000), 0],
+    };
+    let fileHash = await conductor.call(
+      ZOME_NAME,
+      "create_file_metadata",
+      fileMetadata
     );
-    t.ok(chunk);
-    console.log(chunk);
+    t.ok(fileHash);
+
+    console.log("MetaData file Hashe:**********");
+    console.log(fileMetadata);
+    console.log("*********************");
+
+    let fileResult = await conductor.call(
+      ZOME_NAME,
+      "get_file_metadata",
+      fileHash
+    );
+    t.ok(fileResult);
+
+    for (const chunkHash of fileResult.chunksHashes) {
+      let chunk = await conductor.call(ZOME_NAME, "get_file_chunk", chunkHash);
+      t.ok(chunk);
+      console.log(chunk);
+    }
   }
-});
+);
 
 orchestrator.run();
