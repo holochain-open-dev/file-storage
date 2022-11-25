882180ae2cc7cc9b07799d5f10f39541fbde2fcb
Guillem Córdoba <guillem.cordoba@gmail.com>
2022-11-25 14:57:12 +0100

    Bumped hdk

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   holonixPath = (import ./nix/sources.nix).holonix; # points to the current state of the Holochain repository
   holonix = import (holonixPath) {
-   holochainVersionId = "v0_0_173"; # specifies the Holochain version
+   holochainVersionId = "v0_0_174"; # specifies the Holochain version
    rustVersion = {
     track = "stable";
     version = "1.64.0";
