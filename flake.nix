0a31327eae820debeedc540ce62412a1a44b2031
guillem.cordoba <guillem.cordoba@gmail.com>
2024-04-10 12:53:24 +0200

    Added scaffold

--- flake.nix
+++ flake.nix
@@ -41,6 +41,23 @@
                 nodejs_20
               ];
             };
+
+            packages.scaffold = pkgs.symlinkJoin {
+              name = "scaffold-remote-zome";
+              paths = [ inputs'.hc-infra.packages.scaffold-remote-zome ];
+              buildInputs = [ pkgs.makeWrapper ];
+              postBuild = ''
+                wrapProgram $out/bin/scaffold-remote-zome \
+                  --add-flags "profiles \
+                    --integrity-zome-name file_storage_integrity \
+                    --coordinator-zome-name file_storage \
+                    --remote-zome-git-url github:holochain-open-dev/file-storage \
+                    --remote-zome-git-branch nixify \
+                    --remote-npm-package-name @holochain-open-dev/file-storage \
+                    --remote-npm-package-path ui"
+              '';
+            };
+            
           };
       };
 }
