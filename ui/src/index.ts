5b8b49cd5111888e63e7b7b6f9b472b3a4567e53
guillem.cordoba <guillem.cordoba@gmail.com>
2024-07-18 23:30:42 +0200

    Export dropzone

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -2,3 +2,4 @@ export * from "./file-storage-client.js";
 export * from "./holochain-dropzone.js";
 export * from "./types.js";
 export * from "./context.js";
+export * from "@scoped-elements/dropzone";
